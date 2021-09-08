9979627c7a8c059c2951b72796ae3f05e845f72b
befayer <lach2508@mail.ru>
2021-09-08 13:29:03 +0300

    Was added a support of Selenium GRID

--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -4,10 +4,17 @@ import io.qameta.allure.*;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 import org.junit.BeforeClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
@@ -21,24 +28,31 @@ public class LoginTest {
     public static final String PASSWORD = "helloiampassword";
     public static final String MAIL = "simbirsofttestapp@yandex.ru";
 
-    @BeforeClass
-    public static void setup(){
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\chromedriver\\chromedriver.exe");
-        driver = new ChromeDriver();
+    @BeforeClass
+    public static void setup() throws MalformedURLException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        //DesiredCapabilities cap = DesiredCapabilities.chrome();
+        capabilities.setPlatform(Platform.WINDOWS);
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("92.0.4515.107");
+        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.0.105:5558/wd/hub"), capabilities);
+        //System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\chromedriver\\chromedriver.exe");
+        //driver = new ChromeDriver();
         loginPage = new LoginPage(driver);
         mailPage = new MailPage(driver);
         profilePage = new ProfilePage(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://passport.yandex.ru/auth");
-
     }
 
     @Epic("Тестирование работы автотеста как тестового задания для Simbir Soft")
     @Description("Тест проверяет, что сообщения с темой есть") //
     @Test
     public void countMessageTest(){
+
 
         //ввод логина
         loginPage.inputLogin(LOGIN);
